ae3eb1993c7514d29bc9ad24323393ac48b27e0d
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-13 14:02:17 +0200

    Shopping.API project added.

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "asd"
+            })
+            .ToArray();
+        }
+    }
+}
