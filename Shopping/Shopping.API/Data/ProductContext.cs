4a3822effe882cb4f02ae46bb0200edda14aa85c
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-18 14:51:19 +0200

    Connected to local Mongo DB container

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -1,17 +1,44 @@
 ﻿using Shopping.API.Models;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace Shopping.API.Data
 {
-    public static class ProductContext
+    public class ProductContext
     {
-        public static readonly List<Product> Products = new List<Product>
+        public ProductContext(IConfiguration configuration)
         {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+
+            SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+
+        private void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
                 new Product()
                 {
                     Id = "602d2149e773f2a3990b47f5",
                     Name = "IPhone X",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
                     Category = "Smart Phone"
@@ -20,7 +47,8 @@ namespace Shopping.API.Data
                 {
                     Id = "602d2149e773f2a3990b47f6",
                     Name = "Samsung 10",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-2.png",
                     Price = 840.00M,
                     Category = "Smart Phone"
@@ -29,7 +57,8 @@ namespace Shopping.API.Data
                 {
                     Id = "602d2149e773f2a3990b47f7",
                     Name = "Huawei Plus",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-3.png",
                     Price = 650.00M,
                     Category = "White Appliances"
@@ -38,7 +67,8 @@ namespace Shopping.API.Data
                 {
                     Id = "602d2149e773f2a3990b47f8",
                     Name = "Xiaomi Mi 9",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-4.png",
                     Price = 470.00M,
                     Category = "White Appliances"
@@ -47,7 +77,8 @@ namespace Shopping.API.Data
                 {
                     Id = "602d2149e773f2a3990b47f9",
                     Name = "HTC U11+ Plus",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-5.png",
                     Price = 380.00M,
                     Category = "Smart Phone"
@@ -56,12 +87,14 @@ namespace Shopping.API.Data
                 {
                     Id = "602d2149e773f2a3990b47fa",
                     Name = "LG G7 ThinQ",
-                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    Description =
+                        "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-6.png",
                     Price = 240.00M,
                     Category = "Home Kitchen"
                 }
-        };
+            };
+        }
     }
 }
 
